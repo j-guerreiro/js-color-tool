7403354ac98d020d1c1ece88dd1d9ff031eb02d9
jguerreiro <joel.borghi@gmail.com>
2024-02-15 20:48:16 -0300

    Playing with parseInt and checking values outputs

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const colorInput = document.querySelector('.color-input');
 const colorBoxIn = document.querySelector('.color-box-input');
+const colorBoxOut = document.querySelector('.color-box-output');
 const errorEl = document.querySelector('#error-message');
 
 // Input listener to set up input box bg color.
 colorInput.addEventListener("change", setInputColorBox);
+// Input listener to set up input box bg color.
 
 function setInputColorBox() {
   if (colorInput.value[0] != '#' && colorInput.value != '') {
@@ -16,6 +18,23 @@ function setInputColorBox() {
     errorEl.style.display = 'none';
     colorBoxIn.style.backgroundColor = colorInput.value;
   }
+
+  // removes '#' character from hex input.
+  let hexValue = colorInput.value.slice(1);
+  // Transform the hex base number into dec.
+  let decValue = parseInt(hexValue, 16);
+  console.log(decValue * 0.5);
+  colorBoxOut.style.backgroundColor = colorInput.value;
+
+  // todo - find a way to separate the rgb values from the string.
+  let rawRGBValue = colorBoxOut.style.backgroundColor;
+  let cleanRGBValue = rawRGBValue.slice(4, 17).trim();
 }
 
+/* 
+Todo:
+Add 2 listeners: 
+1. Lighten/darken switch
+2. Color range
+*/
 
